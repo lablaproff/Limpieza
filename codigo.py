3088f3a18524568cacfb8f72dd4aff440b9713bd
lablaproff <lablaproff@gmail.com>
2025-06-18 09:36:09 -0500

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -11,7 +11,7 @@ tamano_lote = st.number_input("Ingrese el tamaño del lote del producto B (canti
 num_dosis = st.number_input("Ingrese el número de dosis máx del producto B", min_value=0)
 area_total = st.number_input("Ingrese el área total del tren de equipo (cm²)", min_value=0.0, format="%.2f")
 tamano_lotekg = st.number_input("Ingrese el tamaño de lote producto B (kg)", min_value=0.0, format="%.2f")
-tamano_lotemg = st.number_input("Ingrese el tamanño de lote producto B (mg)", min_value=0.0, format="%.2f")
+tamano_lotemg = st.number_input("Ingrese el tamaño de lote producto B (mg)", min_value=0.0, format="%.2f")
 dl50 = st.number_input("Ingrese el Dl50 Producto A", min_value=0, format="%.2f")
 nombre_tableta = st.text_input("Ingrese el nombre del producto")
 
